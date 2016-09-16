dc7c6709083b13bacd18ce85445c159eb166fcd2
Ruth Mills <ruth.mills@connect-group.com>
2016-09-16 18:07:23 +0100

    Tidy up the code a bit.

--- src/main/java/uk/me/ruthmills/batbox/thermostat/service/impl/ThermostatServiceImpl.java
+++ src/main/java/uk/me/ruthmills/batbox/thermostat/service/impl/ThermostatServiceImpl.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import uk.me.ruthmills.batbox.thermostat.model.TemperatureAndHumidityBean;
@@ -13,14 +14,24 @@ import uk.me.ruthmills.batbox.thermostat.service.ThermostatService;
 
 @Service
 public class ThermostatServiceImpl implements ThermostatService {
+	
+	private static final String LOLDHT_PATH = "/usr/local/bin/loldht";
+	private static final String GPIO_PORT = "8";
+	private static final String NUM_RETRIES = "5";
+	
+	private static final Logger LOGGER = Logger.getLogger(ThermostatServiceImpl.class);
 
 	@Override
 	public TemperatureAndHumidityBean readTemperatureAndHumidity() {
+		return invokeLoldht();
+	}
+	
+	private synchronized TemperatureAndHumidityBean invokeLoldht() {
 		TemperatureAndHumidityBean temperatureAndHumidityBean = new TemperatureAndHumidityBean();
 		
 		InputStream stdout = null;
 		try {
-			ProcessBuilder processBuilder = new ProcessBuilder("/usr/local/bin/loldht", "8", "5");
+			ProcessBuilder processBuilder = new ProcessBuilder(LOLDHT_PATH, GPIO_PORT, NUM_RETRIES);
 			processBuilder.redirectErrorStream(true);
 			Process process = processBuilder.start();
 			stdout = process.getInputStream();
@@ -30,28 +41,28 @@ public class ThermostatServiceImpl implements ThermostatService {
 			do
 			{
 				line = reader.readLine();
-				System.out.println("Line: " + line);
+				LOGGER.trace("Line: " + line);
 				if ((line != null) && line.startsWith("Humidity")) {
 					String humidity = line.substring("Humidity = ".length(), line.indexOf("%") - 1);
 					String temperature = line.substring(line.indexOf("Temperature = ") + "Temperature = ".length(), line.indexOf("*") - 1);
-					System.out.println("Humidity: <" + humidity + ">");
-					System.out.println("Temperature: <" + temperature + ">");
+					LOGGER.debug("Humidity: <" + humidity + ">");
+					LOGGER.debug("Temperature: <" + temperature + ">");
 					temperatureAndHumidityBean.setHumidity(new BigDecimal(humidity));
 					temperatureAndHumidityBean.setTemperature(new BigDecimal(temperature));
 				}
 			} while (line != null);
 		} catch (Exception ex) {
-			ex.printStackTrace();
+			LOGGER.error(ex);
 		} finally {
 			if (stdout != null) {
 				try {
 					stdout.close();
 				} catch (IOException ex) {
-					ex.printStackTrace();
+					LOGGER.error(ex);
 				}
 			}
 		}
 		
-		return temperatureAndHumidityBean;
+		return temperatureAndHumidityBean;		
 	}
 }
