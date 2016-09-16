14be12bed3e5709b2669021021c81aac438db335
Ruth Mills <ruth.mills@connect-group.com>
2016-09-16 17:52:20 +0100

    Test version of reading temperature and humidity from loldht

--- src/main/java/uk/me/ruthmills/batbox/thermostat/service/impl/ThermostatServiceImpl.java
+++ src/main/java/uk/me/ruthmills/batbox/thermostat/service/impl/ThermostatServiceImpl.java
@@ -1,5 +1,9 @@
 package uk.me.ruthmills.batbox.thermostat.service.impl;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
 
 import org.springframework.stereotype.Service;
@@ -13,8 +17,39 @@ public class ThermostatServiceImpl implements ThermostatService {
 	@Override
 	public TemperatureAndHumidityBean readTemperatureAndHumidity() {
 		TemperatureAndHumidityBean temperatureAndHumidityBean = new TemperatureAndHumidityBean();
-		temperatureAndHumidityBean.setTemperature(new BigDecimal("21.5"));
-		temperatureAndHumidityBean.setHumidity(new BigDecimal("33.6"));
+		
+		InputStream stdout = null;
+		try {
+			ProcessBuilder processBuilder = new ProcessBuilder("/usr/local/bin/loldht", "8", "5");
+			processBuilder.redirectErrorStream(true);
+			Process process = processBuilder.start();
+			stdout = process.getInputStream();
+			process.waitFor();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(stdout));
+			String line = null;
+			do
+			{
+				line = reader.readLine();
+				System.out.println("Line: " + line);
+				if ((line != null) && line.startsWith("Humidity")) {
+					String humidity = line.substring("Humidity = ".length(), line.indexOf("%") - 1);
+					String temperature = line.substring(line.indexOf("Temperature = ") + "Temperature = ".length(), line.indexOf("*") - 1);
+					temperatureAndHumidityBean.setHumidity(new BigDecimal(humidity));
+					temperatureAndHumidityBean.setTemperature(new BigDecimal(temperature));
+				}
+			} while (line != null);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			if (stdout != null) {
+				try {
+					stdout.close();
+				} catch (IOException ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+		
 		return temperatureAndHumidityBean;
 	}
 }
